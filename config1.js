69781f3c430e1065dfd3bd8e5e185dc5e4cbfbfc
dragonxd55 <37715638+dragonxd55@users.noreply.github.com>
2018-05-13 15:01:04 +0200

    Update config1.js

--- config1.js
+++ config1.js
@@ -1,6 +1,6 @@
 
 exports.TOKEN = 'process.env.BOT_TOKEN'; // حط توكن البوت هنا
 
-exports.PREFIX = '2';//برفكس البوت هو يلي بيشغل لاغاني
+exports.PREFIX = '3';//برفكس البوت هو يلي بيشغل لاغاني
 
 exports.GOOGLE_API_KEY = 'AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8'; // لا تغيره
